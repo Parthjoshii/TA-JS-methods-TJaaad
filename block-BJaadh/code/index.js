ee2adffcaca163bde1e9ccb0da71921a5bc53ffc
Parth Joshi <jparth1805@gmail.com>
2023-04-03 15:40:46 +0530

    block-BJaadh

--- block-BJaadh/code/index.js
+++ block-BJaadh/code/index.js
@@ -31,36 +31,63 @@ strings.shift ()
 console.log(strings)
 
 // - Find all the words that contain 'is' use string method 'includes'
-
+let checkIs = strings.filter((strings) => strings.includes("is"))
+console.log(checkIs)
 
 // - Find all the words that contain 'is' use string method 'indexOf'
+let checkIsIndexOf = strings.filter((strings) => strings.indexOf("is"))
+console.log(checkIs) !== -1
 
 // - Check if all the numbers in numbers array are divisible by three use array method (every)
-
-// -  Sort Array from smallest to largest
+numbers.every(num => num % 3 === 0);
+// -  Sort Array from smallest to  largest
+[...numbers].sort((a,b) => a - b)
 
 // - Remove the last word in strings
 console.log(strings.pop())
 
 // - Find largest number in numbers
+let largest = [...numbers].sort((a,b) => a - b).pop();
 
 // - Find longest string in strings
+let longest = [...strings].sort((a,b) => a.length - b.length).pop()
 
 // - Find all the even numbers
 
+let even = numbers.filter((num) => num % 2 === 0)
 // - Find all the odd numbers
-
+let odd = numbers.filter((num) => num % 2 !== 0)
 // - Place a new word at the start of the array use (unshift)
+strings.unshift("WOrd")
 
 // - Make a subset of numbers array [18,9,7,11]
+numbers.slice(3 , 7)
+
 
 // - Make a subset of strings array ['a','collection']
+strings.slice(2,3)
 
 // - Replace 12 & 18 with 1221 and 1881
+console.log(numbers.map((num) => {
+  if (num === 12) {
+    return 1221;
+  } else if (num === 18) {
+    return 1881
+  } else {
+    return num
+  }
+}))
 
 // - Replace words in strings array with the length of the word
+let stringsLength = (
+  strings.map((strings) => strings.length)
+) 
 
 // - Find the sum of the length of words using above question
+stringsLength.reduce((acc, cv) => {
+  acc = acc + cv;
+  return acc
+},0)
 
 // - Customers Array
 var customers = [
@@ -70,6 +97,7 @@ var customers = [
   { firstname: 'Jack', lastname: 'White' },
 ];
 // - Find all customers whose firstname starts with 'J'
+let filteredCustomer = customers.filter((customers) => customers.firstname.startsWith("J"));
 
 // - Create new array with only first name
 
